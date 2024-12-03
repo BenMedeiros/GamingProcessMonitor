92a21857e3c85def0846138ed64577977acda1c4
Ben <ben.medeiros@planttherapy.com>
2024-12-02 22:47:11 -0500

    wow powershell is super annoying

--- Scripts/Script1.ps1
+++ Scripts/Script1.ps1
@@ -17,7 +17,37 @@ $watcher.NotifyFilter = [System.IO.NotifyFilters]::LastWrite
 $sourceIdentifier = "FileChangedEvent"
 Write-Host "Registering event with SourceIdentifier: $sourceIdentifier"
 $changedEventHandler = Register-ObjectEvent $watcher "Changed" -SourceIdentifier $sourceIdentifier -Action {
-    Write-Host "File Changed: $($Event.SourceEventArgs.FullPath)"
+    Write-Host "File Changed: "
+    
+    function Increment-ModuleVersion {
+        param (
+            [string]$filePath
+        )
+
+        Write-Host "Incrementing ModuleVersion in file: $filePath"
+        $content = Get-Content -Path $filePath
+        $moduleVersionLine = $content | Select-String -Pattern 'ModuleVersion'
+        if ($moduleVersionLine) {
+            Write-Host "ModuleVersion found in the file."
+            $version = $moduleVersionLine -replace '.*?(\d+\.\d+\.\d+\.\d+).*', '$1'
+            $versionParts = $version -split '\.'
+            
+            Write-Host "Current ModuleVersion: $version Parts: $versionParts[0] $versionParts[1] $versionParts[2] $versionParts[3]"
+            $versionPartMinorNum = $versionParts[2] -split ''''
+            $versionPartMinorNum = [int]$versionPartMinorNum[0] + 1
+            $versionParts[2] = $versionPartMinorNum -join ''''
+            $newVersion = $versionParts -join '.'
+            Write-Host "New ModuleVersion: $newVersion"
+            $newContent = $content -replace $version, $newVersion
+            Set-Content -Path $filePath -Value $newContent
+            Write-Host "ModuleVersion incremented to $newVersion"
+        } else {
+            Write-Host "ModuleVersion not found in the file."
+        }
+    }
+
+    $resolvedFilePath = (Resolve-Path -Path "$PSScriptRoot\..\Modules\MyModule\MyModule.psd1").Path
+    Increment-ModuleVersion -filePath $resolvedFilePath
 }
 
 # Begin monitoring the directory
